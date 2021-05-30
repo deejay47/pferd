f1eac47391bc47d8f0622384aa19451fa7c4d007
DJ Romero <djrs2247@gmail.com>
2021-05-30 03:47:42 -0300

    Se agrega footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import NavBar from "./components/NavBar/NavBar"
+import Footer from "./components/Footer/Footer"
 
 import Home from "./views/Home/Home"
 import CategoryItems from "./views/CategoryItems/CategoryItems"
@@ -28,6 +29,8 @@ function App() {
             <NotFound></NotFound>
           </Route>
         </Switch>
+        <Footer></Footer>
+
       </BrowserRouter>
 
     </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="py-4 bg-light flex-shrink-0">
+      <div className="container text-center">
+        <span className="text-muted">2021 - Pferd MotoAccesorios</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
