53c403371356fc97d316198f0386dd8735c3052b
DJ Romero <djrs2247@gmail.com>
2021-05-30 01:33:31 -0300

    Agregado de routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import Home from "./views/Home/Home"
-import ItemDetails from "./views/ItemDetails/ItemDetails";
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import NavBar from "./components/NavBar/NavBar"
 
+import Home from "./views/Home/Home"
+import CategoryItems from "./views/CategoryItems/CategoryItems"
+import ItemDetails from "./views/ItemDetails/ItemDetails"
+import NotFound from "./views/NotFound/NotFound"
+
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
         <NavBar></NavBar>
-
         <Switch>
           <Route exact path="/">
             <Home></Home>
           </Route>
+          <Route exact path="/category/:id">
+            <CategoryItems></CategoryItems>
+          </Route>
           <Route exact path="/item/:id">
             <ItemDetails></ItemDetails>
           </Route>
-          <Route path="/category/:id">
-            <h1>categorias!</h1>
-          </Route>
           <Route>
-            <h1>404!</h1>
+            <NotFound></NotFound>
           </Route>
         </Switch>
       </BrowserRouter>
