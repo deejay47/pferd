db3407786dbbb1d76907aed1b2bd82ee128d29ae
DJ Romero <djrs2247@gmail.com>
2021-05-30 01:47:34 -0300

    Nuevas vistas

---
+++ src/views/CategoryItems/CategoryItems.js
@@ -0,0 +1,13 @@
+import ItemListContainer from "../../components/ItemListContainer/ItemListContainer"
+
+function CategoryItems() {
+  return (
+    <div className="category-items-view">
+      
+      <ItemListContainer></ItemListContainer>
+
+    </div>
+  );
+}
+
+export default CategoryItems;
--- src/views/ItemDetails/ItemDetails.js
+++ src/views/ItemDetails/ItemDetails.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { useParams } from "react-router";
 import ItemDetailContainer from "../../components/ItemDetailContainer/ItemDetailContainer"
 
 import "./ItemDetails.scss";
 
 function ItemDetails() {
 
-  const params = useParams()
-  const itemId = params.id
-
   return (
 
     <div id="item-details-view">
 
-      <ItemDetailContainer itemId={itemId}></ItemDetailContainer>
+      <ItemDetailContainer></ItemDetailContainer>
 
     </div>
 
