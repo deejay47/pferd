11c8f9f5fa5e595e3dc0256334ba02927510b748
DJ Romero <djrs2247@gmail.com>
2021-05-30 01:35:23 -0300

    Correcciones lista

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,34 +1,43 @@
 import React, { useState } from "react";
-import { Card, Button, Col } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ItemCount from "../ItemCount/ItemCount";
 
 import "./Item.scss";
 
-
 function Item(props) {
-
-  const [quantity, setQuantity] = useState(Number)
+  const [quantity, setQuantity] = useState(Number);
 
   const quantityUpdate = (selectedQuantity) => {
-    setQuantity(selectedQuantity)
+    setQuantity(selectedQuantity);
     console.log(selectedQuantity);
-  }
+  };
 
   return (
-    <Col>
       <Card className="text-center">
-        <Card.Img variant="top" src={props.item.pictureUrl} />
         <Card.Body>
+          <Card.Title className="container" as={Link} to={"/item/" + props.item.id}>
+            <Card.Img variant="top" src={props.item.pictureUrl} className="image" />
+            <div className="overlay">
+              <div className="text">Más detalles...</div>
+            </div>
+          </Card.Title>
           <Card.Title>{props.item.title}</Card.Title>
-          <Card.Text>
-            {props.item.description}
-          </Card.Text>
           <p className="price-tag">$ {props.item.price}</p>
-          <ItemCount stock={props.item.stock} onChange={quantityUpdate} selectedQuantity={quantity}></ItemCount>
-          <Button variant="primary" value={props.item.id} disabled={props.item.stock === 0}>{props.item.stock !== 0 ? "Agregar al carrito" : "Sin stock"}</Button>
+          <ItemCount
+            stock={props.item.stock}
+            onChange={quantityUpdate}
+            selectedQuantity={quantity}
+          ></ItemCount>
+          <Button
+            variant="primary"
+            value={props.item.id}
+            disabled={props.item.stock === 0}
+          >
+            {props.item.stock !== 0 ? "Agregar al carrito" : "Sin stock"}
+          </Button>
         </Card.Body>
       </Card>
-    </Col>
   );
 }
 
