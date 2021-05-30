4b996d9e0646764a236715842435e97d6e8f6e91
DJ Romero <djrs2247@gmail.com>
2021-05-30 01:35:44 -0300

    Loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { Container, Row, Alert, Spinner } from "react-bootstrap";
 
+import './Loading.scss'
+
 function Loading() {
 
     return (
-        <Container fluid="md">
+        <Container fluid="md" className="loading">
             <Row className="justify-content-md-center">
 
                 <Alert key="1" variant="light" className="text-center">
                     <Alert.Heading>Cargando</Alert.Heading>
-                    <br></br>
-
+                    <br/>
                     <Spinner animation="border" variant="danger" role="status">
                     </Spinner>
-                    <br></br>
-                    <br></br>
-
+                    <br/><br/>
                     <p>Aguarde por favor</p>
-
                 </Alert>
 
             </Row>
