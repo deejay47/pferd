50181dd181f6a73e623b47ec74d916b638490296
DJ Romero <djrs2247@gmail.com>
2021-07-02 17:54:37 -0300

    Adición de nuevas vistas y rutas

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ import CategoryItems from "./views/CategoryItems/CategoryItems"
 import ItemDetails from "./views/ItemDetails/ItemDetails"
 import NotFound from "./views/NotFound/NotFound"
 import Cart from './views/Cart/Cart'
+import Checkout from './views/Checkout/Checkout'
+import OrderPayment from './views/OrderPayment/OrderPayment'
+import Resellers from './views/Resellers/Resellers'
+import About from './views/About/About'
 
 /* Contexts */
 import {CartProvider} from "./contexts/cartContext"
 
 import './App.scss'
-import Checkout from './views/Checkout/Checkout'
+import ConfirmedOrder from './views/ConfirmedOrder/ConfirmedOrder'
 
 function App() {
 
@@ -31,6 +35,12 @@ function App() {
           <Route exact path="/">
             <Home></Home>
           </Route>
+          <Route exact path="/resellers">
+            <Resellers></Resellers>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
           <Route exact path="/category/:id">
             <CategoryItems></CategoryItems>
           </Route>
@@ -42,6 +52,12 @@ function App() {
           </Route>
           <Route exact path="/checkout">
             <Checkout></Checkout>
+          </Route>
+          <Route exact path="/payment/:id">
+            <OrderPayment></OrderPayment>
+          </Route>
+          <Route exact path="/order/:id">
+            <ConfirmedOrder></ConfirmedOrder>
           </Route>
           <Route>
             <NotFound></NotFound>
